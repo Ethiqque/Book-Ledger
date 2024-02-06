7d6b7898c9765f88f0d0be21625485a108bfeb3f
ethiqque <danielpyld.work@gmail.com>
2024-02-06 17:06:31 +0200

    BookLedger Spring MVC program

---
+++ src/main/java/com/ethiqque/springcourse/models/Person.java
@@ -0,0 +1,51 @@
+package com.ethiqque.springcourse.models;
+
+import com.ethiqque.springcourse.util.PersonValidator;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min = 2, max = 100, message = "Name size should be from 2 to 100 characters")
+    private String fullName;
+
+    @Min(value = 1900, message = "Year of birth should be more, than 1900")
+    private int yearOfBirth;
+
+    public Person() {
+
+    }
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
